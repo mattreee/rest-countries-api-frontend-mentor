9187a947a63972393a06a1ae786b192fe1dc5ee6
mattreee <fantinatomatheuss@gmail.com>
2022-08-16 17:33:09 -0400

    feat: filtering through search

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -2,13 +2,19 @@ import { useCountries } from "../../State";
 import ListItem from "../ListItem";
 import { ListStyles } from "./list.styled";
 
-const Index = () => {
+const Index = ({ searchInput }: any) => {
 	const { countries } = useCountries();
+
+	const searchList = countries.filter((elem: any) =>
+		elem.name.common.toLowerCase().includes(searchInput)
+	);
+
+	const listToBeRendered = searchInput ? searchList : countries;
 
 	return (
 		<ListStyles>
 			<ul className="countries__ul">
-				{countries?.map((elem: any) => {
+				{listToBeRendered?.map((elem: any) => {
 					return (
 						<ListItem
 							key={elem.name.common}
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,21 +1,27 @@
+import { ChangeEvent, useState } from "react";
 import SearchBar from "../SearchBar";
 import FilterWindow from "../FilterWindow";
 import List from "../List";
-
 import { MainStyles } from "./main.styled";
 
-const index = () => {
+const Index = () => {
+	const [searchInput, setSearchInput] = useState("");
+
+	const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+		setSearchInput(e.target.value);
+	};
+
 	return (
 		<MainStyles>
 			<div className="main__filter-options">
-				<SearchBar />
+				<SearchBar handleSearch={handleSearch} />
 				<FilterWindow />
 			</div>
 			<div>
-				<List />
+				<List searchInput={searchInput} />
 			</div>
 		</MainStyles>
 	);
 };
 
-export default index;
+export default Index;
